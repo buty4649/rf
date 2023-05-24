36f7f24398b458f5e36bc811d4b13662c35463d3
buty4649 <buty4649@gmail.com>
2023-05-25 05:18:18 +0900

    Add test to yaml filter

--- spec/filter/yaml_spec.rb
+++ spec/filter/yaml_spec.rb
@@ -1,59 +1,57 @@
-describe 'JSON filter', type: :aruba do
+describe 'YAML filter', type: :aruba do
   context 'Input from stdin' do
     describe 'Output string' do
-      let(:input) { load_fixture('json/string.json') }
-      let(:output) { '"test"' }
-      before { run_rf('-j true', input) }
+      let(:input) { load_fixture('yaml/string.yml') }
+      let(:output) { 'test' }
+      before { run_rf('-y true', input) }
 
       it { expect(last_command_started).to be_successfully_executed }
       it { expect(last_command_started).to have_output output_string_eq output }
     end
 
     describe 'Output each object of the array one by one' do
-      let(:input) { load_fixture('json/array.json') }
+      let(:input) { load_fixture('yaml/array.yml') }
       let(:output) do
         <<~OUTPUT
-          "foo"
-          "bar"
-          "baz"
+          foo
+          bar
+          baz
         OUTPUT
       end
-      before { run_rf('-j true', input) }
+      before { run_rf('-y true', input) }
 
       it { expect(last_command_started).to be_successfully_executed }
       it { expect(last_command_started).to have_output output_string_eq output }
     end
 
     describe 'Output only the filtered objects' do
-      let(:input) { load_fixture('json/array.json') }
-      let(:output) { '"foo"' }
-      before { run_rf('-j /foo/', input) }
+      let(:input) { load_fixture('yaml/array.yml') }
+      let(:output) { 'foo' }
+      before { run_rf('-y /foo/', input) }
 
       it { expect(last_command_started).to be_successfully_executed }
       it { expect(last_command_started).to have_output output_string_eq output }
     end
 
     describe 'Output the value of the selected Hash key' do
-      let(:input) { load_fixture('json/hash.json') }
+      let(:input) { load_fixture('yaml/hash.yml') }
       let(:output) do
         <<~OUTPUT
-          [
-            "a",
-            "b",
-            "c"
-          ]
+          - a
+          - b
+          - c
         OUTPUT
       end
-      before { run_rf('-j "_.bar.baz"', input) }
+      before { run_rf('-y "_.bar.baz"', input) }
 
       it { expect(last_command_started).to be_successfully_executed }
       it { expect(last_command_started).to have_output output_string_eq output }
     end
 
     describe 'Output the value of the selected Hash space included key' do
-      let(:input) { load_fixture('json/hash.json') }
-      let(:output) { '"foo bar"' }
-      before { run_rf(%q(-j '_["foo bar"]'), input) }
+      let(:input) { load_fixture('yaml/hash.yml') }
+      let(:output) { 'foo bar' }
+      before { run_rf(%q(-y '_["foo bar"]'), input) }
 
       it { expect(last_command_started).to be_successfully_executed }
       it { expect(last_command_started).to have_output output_string_eq output }
@@ -62,11 +60,11 @@ describe 'JSON filter', type: :aruba do
 
   context 'Input from file' do
     describe 'Output string' do
-      let(:file) { 'test.json' }
-      let(:input) { load_fixture('json/string.json') }
-      let(:output) { '"test"' }
+      let(:file) { 'test.yml' }
+      let(:input) { load_fixture('yaml/string.yml') }
+      let(:output) { 'test' }
       before { write_file file, input }
-      before { run_rf("-j true #{file}") }
+      before { run_rf("-y true #{file}") }
 
       it { expect(last_command_started).to be_successfully_executed }
       it { expect(last_command_started).to have_output output_string_eq output }
@@ -75,9 +73,9 @@ describe 'JSON filter', type: :aruba do
 
   context 'Suppress automatic printing' do
     describe 'Output string' do
-      let(:input) { load_fixture('json/string.json') }
+      let(:input) { load_fixture('yaml/string.yml') }
       let(:output) { '' }
-      before { run_rf('-q -j true', input) }
+      before { run_rf('-q -y true', input) }
 
       it { expect(last_command_started).to be_successfully_executed }
       it { expect(last_command_started).to have_output output_string_eq output }
