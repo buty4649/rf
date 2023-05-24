6cd8afa47e169ebe18e1970961ca17425d628714
buty4649 <buty4649@gmail.com>
2023-05-25 04:50:34 +0900

    Add test to json filter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,11 @@ $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 Dir.glob(File.expand_path('support/*.rb', __dir__)).each { |f| require_relative f }
 Dir.glob(File.expand_path('support/**/*.rb', __dir__)).each { |f| require_relative f }
 
+def load_fixture(path)
+  fixture = File.expand_path("fixtures/file/#{path}", __dir__)
+  File.read(fixture)
+end
+
 RSpec.configure do |config|
   # need each parallel test to have its own working directory
   config.include Aruba::Api
