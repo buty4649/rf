fe7f05bb4f485c2473f15ee12914a1791b449e60
buty4649 <buty4649@gmail.com>
2023-10-16 01:39:54 +0900

    Aligned Windows build flags with other environments.

--- build_config.rb
+++ build_config.rb
@@ -98,6 +98,7 @@ if build_targets.include?('windows-amd64')
       cc.command = "#{ccache}#{conf.host_target}-gcc-posix"
       cc.flags << '-static'
     end
+    conf.cc.defines      += %w[MRB_STR_LENGTH_MAX=0 MRB_UTF8_STRING MRUBY_YAML_NO_CANONICAL_NULL]
     conf.cxx.command      = "#{ccache}#{conf.host_target}-g++"
     conf.archiver.command = "#{ccache}#{conf.host_target}-gcc-ar"
 
