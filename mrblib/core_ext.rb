9dde7a30c89a4a03ced30c740b5c28e05475d42f
buty4649 <buty4649@gmail.com>
2025-10-15 15:17:55 +0900

    refactor: replace custom color methods with mruby-terminal-color

--- mrblib/core_ext.rb
+++ mrblib/core_ext.rb
@@ -1,16 +1,4 @@
 class String
-  def red
-    "\e[31m#{self}\e[0m"
-  end
-
-  def magenta
-    "\e[35m#{self}\e[0m"
-  end
-
-  def cyan
-    "\e[36m#{self}\e[0m"
-  end
-
   def try_to_i
     Integer(self)
   rescue ArgumentError
